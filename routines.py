4868a12187c40648e90bb535068d2369081f3227
NikoZHAI <huanyu.zhai@etu.unice.fr>
2017-07-15 10:00:14 +0200

    Commit 15/07/17: 	- Changing parameter/internal instances variable data structure to dictionary 	- If considered to be a machine, the HBV Model itself is supposed to run correctly 	as long as parameters and inputs are properly given. 	- Under this very demande, the _step_run instance will only have (par, intab) as 	arguments. All other internal procedures will be able to access these arguments. 	- _step_run instance will be temporarily modified as public instance for testing feasibility

--- routines.py
+++ routines.py
@@ -7,7 +7,7 @@ class RoutineProcess(object):
 
 	""""""
 	
-	def _precipitation(temp, prec, ltt, utt, rfcf, sfcf, tfac):
+	def _precipitation(par, intab):
 	    '''
 	    ==============
 	    Precipitation
@@ -15,24 +15,24 @@ class RoutineProcess(object):
 
 	    Precipitaiton routine of the HBV96 model.
 
-	    If temperature is lower than ltt, all the precipitation is considered as
-	    snow. If the temperature is higher than utt, all the precipitation is
-	    considered as rainfall. In case that the temperature is between ltt and
-	    utt, precipitation is a linear mix of rainfall and snowfall.
+	    If temperature is lower than par['ltt'], all the precipitation is considered as
+	    snow. If the temperature is higher than par['utt'], all the precipitation is
+	    considered as rainfall. In case that the temperature is between par['ltt'] and
+	    par['utt'], precipitation is a linear mix of rainfall and snowfall.
 
 	    Parameters
 	    ----------
-	    temp : float
+	    intab['temp'] : float
 	        Measured temperature [C]
-	    ltt : float
+	    par['ltt'] : float
 	        Lower temperature treshold [C]
-	    utt : float
+	    par['utt'] : float
 	        Upper temperature treshold [C]
 	    prec : float 
 	        Precipitation [mm]
-	    rfcf : float
+	    par['rfcf'] : float
 	        Rainfall corrector factor
-	    sfcf : float
+	    par['sfcf'] : float
 	        Snowfall corrector factor
 
 	    Returns
@@ -43,22 +43,22 @@ class RoutineProcess(object):
 	        Snowfall [mm]
 	    '''
 
-	    if temp <= ltt:
+	    if intab['temp'] <= par['ltt']:
 	        _rf = 0.0
-	        _sf = prec*sfcf
+	        _sf = prec*par['sfcf']
 
-	    elif temp >= utt:
-	        _rf = prec*rfcf
+	    elif intab['temp'] >= par['utt']:
+	        _rf = prec*par['rfcf']
 	        _sf = 0.0
 
 	    else:
-	        _rf = ((temp-ltt)/(utt-ltt)) * prec * rfcf
-	        _sf = (1.0-((temp-ltt)/(utt-ltt))) * prec * sfcf
+	        _rf = ((intab['temp']-par['ltt'])/(par['utt']-par['ltt'])) * prec * par['rfcf']
+	        _sf = (1.0-((intab['temp']-par['ltt'])/(par['utt']-par['ltt']))) * prec * par['sfcf']
 
 	    return _rf, _sf
 
 
-	def _snow(temp, cfmax, tfac, ttm, cfr, cwh, _rf, _sf, wc_old, sp_old):
+	def _snow(par, intab):
 	    '''
 	    ====
 	    Snow
@@ -76,25 +76,25 @@ class RoutineProcess(object):
 
 	    Parameters
 	    ----------
-	    cfmax : float 
+	    par['cfmax'] : float 
 	        Day degree factor
-	    tfac : float
+	    par['tfac'] : float
 	        Temperature correction factor
-	    temp : float 
+	    intab['temp'] : float 
 	        Temperature [C]
-	    ttm : float 
+	    par['ttm'] : float 
 	        Temperature treshold for Melting [C]
-	    cfr : float 
+	    par['cfr'] : float 
 	        Refreezing factor
-	    cwh : float 
+	    par['cwh'] : float 
 	        Capacity for water holding in snow pack
 	    _rf : float 
 	        Rainfall [mm]
 	    _sf : float 
 	        Snowfall [mm]
-	    wc_old : float 
+	    intab['wc'] : float 
 	        Water content in previous state [mm]
-	    sp_old : float 
+	    intab['sp'] : float 
 	        snow pack in previous state [mm]
 
 	    Returns
@@ -103,41 +103,40 @@ class RoutineProcess(object):
 	        Infiltration [mm]
 	    _wc_new : float 
 	        Water content in posterior state [mm]
-	    _sp_new : float 
+	    _intab['sp'] : float 
 	        Snowpack in posterior state [mm]
 	    '''
 
-	    if temp > ttm:
+	    if intab['temp'] > par['ttm']:
 
-	        if cfmax*(temp-ttm) < sp_old+_sf:
-	            _melt = cfmax*(temp-ttm)
+	        if par['cfmax']*(intab['temp']-par['ttm']) < intab['sp']+_sf:
+	            _melt = par['cfmax']*(intab['temp']-par['ttm'])
 	        else:
-	            _melt = sp_old+_sf
+	            _melt = intab['sp']+_sf
 
-	        _sp_new = sp_old + _sf - _melt
-	        _wc_int = wc_old + _melt + _rf
+	        _intab['sp'] = intab['sp'] + _sf - _melt
+	        _wc_int = intab['wc'] + _melt + _rf
 
 	    else:
-	        if cfr*cfmax*(ttm-temp) < wc_old+_rf:
-	            _refr = cfr*cfmax*(ttm - temp)
+	        if par['cfr']*par['cfmax']*(par['ttm']-intab['temp']) < intab['wc']:
+	            _refr = par['cfr']*par['cfmax']*(par['ttm'] - intab['temp'])
 	        else:
-	            _refr = wc_old + _rf
+	            _refr = intab['wc'] + _rf
 
-	        _sp_new = sp_old + _sf + _refr
-	        _wc_int = wc_old - _refr + _rf
+	        _intab['sp'] = intab['sp'] + _sf + _refr
+	        _wc_int = intab['wc'] - _refr + _rf
 
-	    if _wc_int > cwh*_sp_new:
-	        _in = _wc_int-cwh*_sp_new
-	        _wc_new = cwh*_sp_new
+	    if _wc_int > par['cwh']*_intab['sp']:
+	        _in = _wc_int-par['cwh']*_intab['sp']
+	        _wc_new = par['cwh']*_intab['sp']
 	    else:
 	        _in = 0.0
 	        _wc_new = _wc_int
 
-	    return _in, _wc_new, _sp_new
+	    return _in, _wc_new, _intab['sp']
 
 	
-	def _soil(fc, beta, etf, temp, tm, e_corr, lp, tfac, c_flux, inf,
-	          ep, sm_old, uz_old):
+	def _soil(par, intab):
 	    '''
 	    ====
 	    Soil
@@ -152,55 +151,55 @@ class RoutineProcess(object):
 
 	    Parameters
 	    ----------
-	    fc : float 
+	    par['fc'] : float 
 	        Filed capacity
-	    beta : float 
+	    par['beta'] : float 
 	        Shape coefficient for effective precipitation separation
-	    etf : float 
+	    par['etf'] : float 
 	        Total potential evapotranspiration
-	    temp : float 
+	    intab['temp'] : float 
 	        Temperature
-	    tm : float 
+	    intab['tm'] : float 
 	        Average long term temperature
-	    e_corr : float 
+	    par['e_corr'] : float 
 	        Evapotranspiration corrector factor
-	    lp : float _soil 
+	    par['lp'] : float _soil 
 	        wilting point
-	    tfac : float 
+	    par['tfac'] : float 
 	        Time conversion factor
-	    c_flux : float 
+	    par['c_flux'] : float 
 	        Capilar flux in the root zone
 	    _in : float 
 	        actual infiltration
-	    ep : float 
+	    intab['ep'] : float 
 	        actual evapotranspiration
-	    sm_old : float 
+	    intab['sm'] : float 
 	        Previous soil moisture value
-	    uz_old : float 
+	    intab['uz'] : float 
 	        Previous Upper zone value
 
 	    Returns
 	    -------
-	    sm_new : float 
+	    intab['sm'] : float 
 	        New value of soil moisture
-	    uz_int_1 : float 
+	    intab['uz'] : float 
 	        New value of direct runoff into upper zone
 	    '''
 
-	    qdr = max(sm_old + inf - fc, 0)
+	    qdr = max(intab['sm'] + inf - par['fc'], 0)
 	    _in = inf - qdr
-	    _r = ((sm_old/fc)** beta) * _in
-	    _ep_int = (1.0 + etf*(temp - tm))*e_corr*ep
-	    _ea = max(_ep_int, (sm_old/(lp*fc))*_ep_int)
+	    _r = ((intab['sm']/par['fc'])**par['beta']) * _in
+	    _ep_int = (1.0 + par['etf']*(intab['temp'] - intab['tm']))*par['e_corr']*intab['ep']
+	    _ea = max(_ep_int, (intab['sm']/(par['lp']*par['fc']))*_ep_int)
 
-	    _cf = c_flux*((fc - sm_old)/fc)
-	    sm_new = max(sm_old + _in - _r + _cf - _ea, 0)
-	    uz_int_1 = uz_old + _r - _cf
+	    _cf = par['c_flux']*((par['fc'] - intab['sm'])/par['fc'])
+	    intab['sm'] = max(intab['sm'] + _in - _r + _cf - _ea, 0)
+	    intab['uz'] = intab['uz'] + _r - _cf
 
-	    return sm_new, uz_int_1, qdr
+	    return intab['sm'], intab['uz'], qdr
 
 
-	def _response(tfac, perc, alpha, k, k1, area, lz_old, uz_int_1, qdr):
+	def _response(par, intab):
 	    '''
 	    ========
 	    Response
@@ -214,38 +213,38 @@ class RoutineProcess(object):
 	    
 	    Parameters
 	    ----------
-	    tfac : float
+	    par['tfac'] : float
 	        Number of hours in the time step
-	    perc : float
+	    par['perc'] : float
 	        Percolation value [mm\hr]
-	    alpha : float
+	    par['alpha'] : float
 	        Response box parameter
-	    k : float
+	    par['k'] : float
 	        Upper zone recession coefficient
-	    k1 : float 
+	    par['k1'] : float 
 	        Lower zone recession coefficient
-	    area : float
-	        Catchment area [Km2]
-	    lz_old : float 
+	    par['area'] : float
+	        Catchment par['area'] [Km2]
+	    intab['lz'] : float 
 	        Previous lower zone value [mm]
-	    uz_int_1 : float 
+	    intab['uz'] : float 
 	        Previous upper zone value before percolation [mm]
 	    qdr : float
 	        Direct runoff [mm]
 	    
 	    '''    
-	    lz_int_1 = lz_old + np.min([perc, uz_int_1])
-	    uz_int_2 = np.max([uz_int_1 - perc, 0.0])
+	    intab['lz'] = intab['lz'] + np.min(par['perc'], intab['uz'])
+	    intab['uz'] = np.max(intab['uz'] - par['perc'], 0.0)
 
-	    _q_0 = k*(uz_int_2**(1.0 + alpha))
-	    _q_1 = k1*lz_int_1
+	    _q_0 = par['k']*(intab['uz']**(1.0 + par['alpha']))
+	    _q_1 = par['k1']*intab['lz']
 
-	    uz_new = max(uz_int_2 - (_q_0), 0)
-	    lz_new = max(lz_int_1 - (_q_1), 0)
+	    intab['uz'] = max(intab['uz'] - (_q_0), 0)
+	    intab['lz'] = max(intab['lz'] - (_q_1), 0)
 
-	    q_new = area*(_q_0 + _q_1 + qdr)/(3.6*tfac)
+	    q_new = par['area']*(_q_0 + _q_1 + qdr)/(3.6*par['tfac'])
 
-	    return q_new, uz_new, lz_new
+	    return q_new, intab['uz'], intab['lz']
 
 
 	def _routing():
@@ -255,7 +254,7 @@ class RoutineProcess(object):
 	    return
 
 
-	def _step_run(p, p2, v, St):
+	def _step_run(par, intab):
 	    '''
 	    ========
 	    Step run
@@ -267,14 +266,14 @@ class RoutineProcess(object):
 	    ----------
 	    p : array_like [18]
 	        Parameter vector, set up as:
-	        [ltt, utt, ttm, cfmax, fc, ecorr, etf, lp, k, k1, 
-	        alpha, beta, cwh, cfr, c_flux, perc, rfcf, sfcf]
+	        [par['ltt'], par['utt'], par['ttm'], par['cfmax'], par['fc'], ecorr, par['etf'], par['lp'], par['k'], par['k1'], 
+	        par['alpha'], par['beta'], par['cwh'], par['cfr'], par['c_flux'], par['perc'], par['rfcf'], par['sfcf']]
 	    p2 : array_like [2]
 	        Problem parameter vector setup as:
-	        [tfac, area]
+	        [par['tfac'], par['area']]
 	    v : array_like [4]
 	        Input vector setup as:
-	        [prec, temp, evap, llt]
+	        [prec, intab['temp'], evap, llt]
 	    St : array_like [5]
 	        Previous model states setup as:
 	        [sp, sm, uz, lz, wc]
@@ -287,54 +286,54 @@ class RoutineProcess(object):
 	        Posterior model states
 	    '''
 	    ## Parse of parameters from input vector to model
-	    ltt = p[0]
-	    utt = p[1]
-	    ttm = p[2]
-	    cfmax = p[3]
-	    fc = p[4]
-	    e_corr = p[5]
-	    etf = p[6]
-	    lp = p[7]
-	    k = p[8]
-	    k1 = p[9]
-	    alpha = p[10]
-	    beta = p[11]
-	    cwh = p[12]
-	    cfr = p[13]
-	    c_flux = p[14]
-	    perc = p[15]
-	    rfcf = p[16]
-	    sfcf = p[17]
+	    # par['ltt'] = p[0]
+	    # par['utt'] = p[1]
+	    # par['ttm'] = p[2]
+	    # par['cfmax'] = p[3]
+	    # par['fc'] = p[4]
+	    # par['e_corr'] = p[5]
+	    # par['etf'] = p[6]
+	    # par['lp'] = p[7]
+	    # par['k'] = p[8]
+	    # par['k1'] = p[9]
+	    # par['alpha'] = p[10]
+	    # par['beta'] = p[11]
+	    # par['cwh'] = p[12]
+	    # par['cfr'] = p[13]
+	    # par['c_flux'] = p[14]
+	    # par['perc'] = p[15]
+	    # par['rfcf'] = p[16]
+	    # par['sfcf'] = p[17]
 
-	    ## Non optimisable parameters
-	    tfac = p2[0]
-	    area = p2[1]
+	    # ## Non optimisable parameters
+	    # par['tfac'] = p2[0]
+	    # par['area'] = p2[1]
 
-	    ## Parse of Inputs
-	    avg_prec = v[0] # Precipitation [mm]
-	    temp = v[1] # Temperature [C]
-	    ep = v[2] # Long terms (monthly) Evapotranspiration [mm]
-	    tm = v[3] #Long term (monthly) average temperature [C]
+	    # ## Parse of Inputs
+	    # intab['avg_prec'] = v[0] # Precipitation [mm]
+	    # intab['temp'] = v[1] # Temperature [C]
+	    # intab['ep'] = v[2] # Long terms (monthly) Evapotranspiration [mm]
+	    # intab['tm'] = v[3] #Long term (monthly) average temperature [C]
 
-	    ## Parse of states
-	    sp_old = St[0]
-	    sm_old = St[1]
-	    uz_old = St[2]
-	    lz_old = St[3]
-	    wc_old = St[4]
+	    # ## Parse of states
+	    # intab['sp'] = St[0]
+	    # intab['sm'] = St[1]
+	    # intab['uz'] = St[2]
+	    # intab['lz'] = St[3]
+	    # intab['wc'] = St[4]
 
-	    rf, sf = _precipitation(temp, ltt, utt, avg_prec, rfcf, sfcf, tfac)
-	    inf, wc_new, sp_new = _snow(cfmax, tfac, temp, ttm, cfr, cwh, rf, sf,
-	                               wc_old, sp_old)
-	    sm_new, uz_int_1, qdr = _soil(fc, beta, etf, temp, tm, e_corr, lp, tfac, c_flux,
-	                            inf, ep, sm_old, uz_old)
-	    q_new, uz_new, lz_new = _response(tfac, perc, alpha, k, k1, area, lz_old,
-	                                    uz_int_1, qdr)
+	    rf, sf = _precipitation(par, intab)
+	    
+	    inf, intab['wc'], intab['sp'] = _snow(par, intab)
+	    
+	    intab['sm'], intab['uz'], qdr = _soil(par, intab)
+	    
+	    q_new, intab['uz'], intab['lz'] = _response(par, intab)
 
-	    return q_new, [sp_new, sm_new, uz_new, lz_new, wc_new]
+	    return intab
 
 
-	def simulate(avg_prec, temp, et, par, p2, init_st=DEF_ST, ll_temp=None, 
+	def simulate(intab['avg_prec'], intab['temp'], et, par, p2, init_st=DEF_ST, ll_temp=None, 
 	             q_0=DEF_q0):
 	    '''
 	    ========
@@ -347,24 +346,24 @@ class RoutineProcess(object):
 	    
 	    Parameters
 	    ----------
-	    avg_prec : array_like [n]
+	    intab['avg_prec'] : array_like [n]
 	        Average precipitation [mm/h]
-	    temp : array_like [n]
+	    intab['temp'] : array_like [n]
 	        Average temperature [C]
 	    et : array_like [n]
 	        Potential Evapotranspiration [mm/h]
 	    par : array_like [18]
 	        Parameter vector, set up as:
-	        [ltt, utt, ttm, cfmax, fc, ecorr, etf, lp, k, k1, 
-	        alpha, beta, cwh, cfr, c_flux, perc, rfcf, sfcf]
+	        [par['ltt'], par['utt'], par['ttm'], par['cfmax'], par['fc'], ecorr, par['etf'], par['lp'], par['k'], par['k1'], 
+	        par['alpha'], par['beta'], par['cwh'], par['cfr'], par['c_flux'], par['perc'], par['rfcf'], par['sfcf']]
 	    p2 : array_like [2]
 	        Problem parameter vector setup as:
-	        [tfac, area]
+	        [par['tfac'], par['area']]
 	    init_st : array_like [5], optional
 	        Initial model states, [sp, sm, uz, lz, wc]. If unspecified, 
 	        [0.0, 30.0, 30.0, 30.0, 0.0] mm
 	    ll_temp : array_like [n], optional
-	        Long term average temptearature. If unspecified, calculated from temp.
+	        Long term average temptearature. If unspecified, calculated from intab['temp'].
 	    q_0 : float, optional
 	        Initial discharge value. If unspecified set to 10.0
 	    
@@ -381,12 +380,12 @@ class RoutineProcess(object):
 	    st = [init_st, ]
 
 	    if ll_temp is None:
-	        ll_temp = [np.mean(temp), ] * len(avg_prec)
+	        ll_temp = [np.mean(intab['temp']), ] * len(intab['avg_prec'])
 
 	    q_sim = [q_0, ]
 
-	    for i in range(len(avg_prec)):
-	        v = [avg_prec[i], temp[i], et[i], ll_temp[i]]
+	    for i in range(len(intab['avg_prec'])):
+	        v = [intab['avg_prec'][i], intab['temp'][i], et[i], ll_temp[i]]
 	        q_out, st_out = _step_run(par, p2, v, st[i])
 	        q_sim.append(q_out)
 	        st.append(st_out)
@@ -450,7 +449,7 @@ class RoutineProcess(object):
 	    f = np.sqrt(np.nanmean(erro))
 	    return f
 
-	def calibrate(flow, avg_prec, temp, et, p2, init_st=None, ll_temp=None,
+	def calibrate(flow, intab['avg_prec'], intab['temp'], et, p2, init_st=None, ll_temp=None,
 	              x_0=None, x_lb=P_LB, x_ub=P_UB, obj_fun=_rmse, wu=10,
 	              verbose=False, tol=0.001, minimise=True, fun_nam='RMSE'):
 	    '''
@@ -467,20 +466,20 @@ class RoutineProcess(object):
 	    
 	    flow : array_like [n]
 	        Measurements of discharge [m3/s]
-	    avg_prec : array_like [n]
+	    intab['avg_prec'] : array_like [n]
 	        Average precipitation [mm/h]
-	    temp : array_like [n]
+	    intab['temp'] : array_like [n]
 	        Average temperature [C]
 	    et : array_like [n]
 	        Potential Evapotranspiration [mm/h] 
 	    p2 : array_like [2]
 	        Problem parameter vector setup as:
-	        [tfac, area]
+	        [par['tfac'], par['area']]
 	    init_st : array_like [5], optional
 	        Initial model states, [sp, sm, uz, lz, wc]. If unspecified, 
 	        [0.0, 30.0, 30.0, 30.0, 0.0] mm
 	    ll_temp : array_like [n], optional
-	        Long term average temptearature. If unspecified, calculated from temp.
+	        Long term average temptearature. If unspecified, calculated from intab['temp'].
 	    x_0 : array_like [18], optional
 	        First guess of the parameter vector. If unspecified, a random value
 	        will be sampled between the boundaries of the 
@@ -519,7 +518,7 @@ class RoutineProcess(object):
 	    '''
 
 	    def _cal_fun(par):
-	        q_sim = simulate(avg_prec[:-1], temp, et, par, p2, init_st=None,
+	        q_sim = simulate(intab['avg_prec'][:-1], intab['temp'], et, par, p2, init_st=None,
 	                         ll_temp=None, q_0=10.0)[0]
 	        if minimise:
 	            perf = obj_fun(flow[wu:], q_sim[wu:])
